3f05fed210d766a28142abd45104773bb31c5c2e
PRASHANT PANCHAL <ppa@odoo.com>
2018-12-14 17:18:27 +0530

    [ADD] Product Attribute Hidden V9 Features

---
+++ product_attribute_hidden/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Product Attribute Hidden',
+    'summary': 'Product Attribute Hidden',
+    'description': """
+Product Attribute Hidden V9 Features
+====================================
+* Product Attribute type Hidden V9 Features
+    """,
+    'website': 'https://www.odoo.com/page/website',
+    'depends': ['website_sale'],
+    'category': 'Custom Dev',
+    'data': [
+        'views/template.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
